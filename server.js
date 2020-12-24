8e6860a03291b61a14187e5a655cfffec5b902db
Matthew Gramigna <mgramigna@mitre.org>
2020-12-24 16:02:19 -0500

    Use lb module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const Parser = require('rss-parser');
-const cors = require('cors');
+const express = require("express");
+const Parser = require("rss-parser");
+const cors = require("cors");
+const letterboxd = require("letterboxd");
 
 const parser = new Parser();
 const app = express();
@@ -9,16 +10,22 @@ app.use(cors());
 const port = process.env.PORT || 3000;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
-app.get('/rss', async (req, res) => {
+app.get("/rss", async (req, res) => {
   const user = req.query.user;
   if (!user) {
-    res.status(400).json({
-      error: 'Parameter "user" is required'
+    return res.status(400).json({
+      error: 'Parameter "user" is required',
     });
   }
 
-  const feed = await parser.parseURL(`https://letterboxd.com/${user}/rss`);
-  res.json(feed.items);
+  try {
+    const feed = await letterboxd(user);
+    return res.json(feed);
+  } catch (e) {
+    return res.status(500).json({
+      error: e.message,
+    });
+  }
 });
 
 app.listen(port);
